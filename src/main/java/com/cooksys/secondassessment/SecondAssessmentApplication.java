fec9a30a51a4b5bec29447f5de3ccd57c0f45de5
carolmcmullen <carol_mcmullen@outlook.com>
2017-06-27 12:21:04 -0500

    Initial commit

---
+++ src/main/java/com/cooksys/secondassessment/SecondAssessmentApplication.java
@@ -0,0 +1,12 @@
+package com.cooksys.secondassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondAssessmentApplication.class, args);
+	}
+}
